7a9d18e4da687790cf0f8b6b0d9a55e7e0d5b8ba
Jake <heaney_jake@outlook.com>
2019-01-15 16:05:30 +0000

    render test

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,13 +4,8 @@ import './results.css';
 const fileDownload = require('js-file-download');
 
 class Results extends Component {
-	state = {
-		BrokenLinks: [ 'hello', 'is', 'it', 'me', 'you', 'are', 'looking', 'for' ],
-		LinkNumber: 20,
-		BrokenLinkNumber: 8
-	};
 	render() {
-		const { BrokenLinks, LinkNumber, BrokenLinkNumber } = this.state;
+		const { BrokenLinks, LinkNumber, BrokenLinkNumber } = this.props.results;
 		const WorkingLinks = LinkNumber - BrokenLinkNumber;
 		const donutVal = BrokenLinkNumber / LinkNumber * 100;
 		const data = {
@@ -18,8 +13,8 @@ class Results extends Component {
 			datasets: [
 				{
 					data: [ BrokenLinkNumber, WorkingLinks ],
-					backgroundColor: [ '#FF6384', '#36A2EB' ],
-					hoverBackgroundColor: [ '#FF6384', '#36A2EB' ]
+					backgroundColor: [ '#00d1b2', '#36A2EB' ],
+					hoverBackgroundColor: [ '#00d1b2', '#36A2EB' ]
 				}
 			]
 		};
@@ -27,7 +22,7 @@ class Results extends Component {
 			<div className='resultsContainer'>
 				<section className='resultsMain'>
 					<section className='resultsHeader'>
-						<h1>Results</h1>
+						<h2>Results</h2>
 						<h4>Visual Results Breakdown</h4>
 						<p id='allLinks'>
 							<label htmlFor='allLinks' className='authFormLabel'>
@@ -41,7 +36,7 @@ class Results extends Component {
 							</label>
 							{` ${BrokenLinkNumber}`}
 						</p>
-						{/* {fileDownload(BrokenLinks, 'BrokenLinks.csv')} */}
+						{fileDownload(BrokenLinks, 'BrokenLinks.csv')}
 						<p>{`${donutVal}% of the links are broken`}</p>
 						<section className='chart'>
 							<Doughnut
