0042edab917f7ca7c2cd602a0d47b2fe2b295308
charlie-wild <charliewild@gmail.com>
2019-01-15 12:33:12 +0000

    adding components skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Search from './components/Search';
+import Results from './components/Results';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Placeholder text</p>
+        <Search />
+        <Results />
       </div>
     );
   }
