e04ca2c92c38aef1d3af0c4de7485b84aa4d2413
charlie-wild <charliewild@gmail.com>
2019-01-15 12:51:59 +0000

    adding search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    search: '',
+    err: null
+  }
   render() {
     return (
-      <div>
-        
+      <div className="search">
+        <h2 className="search_header">Enter a URL to Search</h2>
+        <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="What is your url?"
+        id="search"
+        className="search_bar"
+        value={this.state.search}
+        onChange={this.handleChange}
+        required/>
+        </form>
       </div>
     );
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+  }
+
+  handleChange = ({target: { value, id }}) => {
+    this.setState({
+      [id]: value
+    });
   }
 }
 
