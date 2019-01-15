a137d28fb992d741709c5ffc56806a1383f3aec1
Jake <heaney_jake@outlook.com>
2019-01-15 14:00:05 +0000

    basic shell for results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class Results extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
+	state = {
+		BrokenLinks: [ 'hello', 'hi', 'by', 'yes', 'no' ],
+		LinkNumber: 20,
+		BrokenLinkNumber: 14
+	};
+	render() {
+		const { BrokenLinks, LinkNumber, BrokenLinkNumber } = this.state;
+		return (
+			<div className='resultsContainer'>
+				<section className='resultsMain'>
+					<section className='resultsHeader'>
+						<h1>Results</h1>
+						<h3>Visual Results Breakdown</h3>
+						<p id='allLinks'>
+							<label htmlFor='allLinks' className='authFormLabel'>
+								All Links:
+							</label>
+							{LinkNumber}
+						</p>
+						<p id='allLinks'>
+							<label htmlFor='allLinks' className='authFormLabel'>
+								All Links:
+							</label>
+							{BrokenLinkNumber}
+						</p>
+					</section>
+					<section>
+						<h3>Individual Links</h3>
+						{BrokenLinks.map((link) => {
+							return <li key={link}>{link}</li>;
+						})}
+					</section>
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Results;
